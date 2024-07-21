87176a2c0667c112951b96ad9c6c5f9143b7fc0a
icovv <hvasilev00@abv.bg>
2024-07-21 16:36:37 +0300

    creating Edit page and fixing texts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import Profile from './components/profile/Profile'
 import Register from './components/register/Register'
 import Login from './components/login/Login'
 import Details from './components/item-details/Details'
+import Edit from './components/edit-item/Edit'
 function App() {
 
   return (
@@ -21,6 +22,7 @@ function App() {
 
         <Route path='/catalog' element={<Catalog></Catalog>}></Route>
         <Route path='/catalog/details' element={<Details></Details>}></Route>
+        <Route path='/catalog/edit' element={<Edit></Edit>}></Route>
         
         <Route path='/listItem' element={<List></List>}></Route>
         <Route path='/search' element={<Search></Search>}></Route>
---
+++ client/src/components/edit-item/Edit.jsx
@@ -0,0 +1,38 @@
+import styles from './Edit.module.css'
+
+export default function Edit(){
+    return(
+        <main className={styles['main']}>
+            <div className={styles["form-container"]}>
+                <h1 className={styles["header"]}><strong>Edit your Car Part</strong></h1>
+                <form id="create-item-form" className={styles['form']}>
+                    <div className={styles["form-group"]}>
+                        <label className={styles['label']} htmlFor="title">Title</label>
+                        <input className= {styles["input"]}type="text" id="title" name="title" required></input>
+                    </div>
+                    <div className={styles["form-group"]}>
+                        <label className={styles['label']} htmlFor="year">Year</label>
+                        <input className= {styles["input"]}type="number" id="year" name="year" required></input>
+                    </div>
+                    <div className={styles["form-group"]}>
+                        <label className={styles['label']} htmlFor="car-brand">Car Brand</label>
+                        <input className= {styles["input"]}type="text" id="car-brand" name="car-brand" required></input>
+                    </div>
+                    <div className={styles["form-group"]}>
+                        <label className={styles['label']} htmlFor="price">Price</label>
+                        <input className= {styles["input"]}type="number" id="price" name="price" required></input>
+                    </div>
+                    <div className={styles["form-group"]}>
+                        <label className={styles['label']} htmlFor="image-link">Image URL</label>
+                        <input className= {styles["input"]}type="url" id="image-link" name="image-link" required></input>
+                    </div>
+                    <div className={styles["form-group"]}>
+                        <label className={styles['label']} htmlFor="description">Description</label>
+                        <textarea className={styles["textarea"]} id="description" name="description" rows="4" required></textarea>
+                    </div>
+                    <button type="submit" className={styles["button"]}>Edit</button>
+                </form>
+            </div>
+        </main>
+    )
+}
