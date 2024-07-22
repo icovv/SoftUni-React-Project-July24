b7dce1884f3244dfa1dae41bf0d8528a748eab0a
icovv <hvasilev00@abv.bg>
2024-07-22 16:39:26 +0300

    adding the submit handler to Register component

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,18 +1,69 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import useForm from '../../hooks/useForm'
 import styles from './Register.module.css'
 import AuthContext from '../../context/AuthContext';
 
 export default function Register(){
     let {registerHandler} = useContext(AuthContext)
-    let {value, changeHandler,submitHandler} = useForm("register", {
+    let [err,setErr] = useState([]);
+    let {value, changeHandler,} = useForm("register", {
         email:'',
         password:'',
         repass:'',
-    }, registerHandler);
+    });
+
+    let submitHandler = async (e) => {
+        e.preventDefault();
+        let pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
+        let errs = [];
+        if (value.email.trim() == '' || value.password.trim() == ''){
+            errs.push({message:"All fields are required!"})
+            // return alert('All fields are required!');
+        }
+        if(!pattern.test(value.email.trim())){
+            errs.push({message:"You have to type a valid email!"})
+            // return alert('You have to type a valid email!');
+        }
+        if(value.password.trim().length < 4){
+            errs.push({message:'Your password must be at least 4 symbols long!'})
+            // return alert('Your password must be at least 4 symbols long!');
+        }
+        if(value.password.trim() != value.repass.trim()){
+            errs.push({message:'Your passwords should match!'})
+            // return alert('Your passwords should match!');
+        }
+
+        if(errs.length > 0){
+            changeHandler(e,errs);
+            setErr(errs);
+            return;
+        }
+        let {email,password} = value;
+
+        email.trim();
+        password.trim();
+
+        let data = await registerHandler(email,password);
+
+        if (data.message){
+            changeHandler(e,[{message:data.message}]);
+            setErr([{message:data.message}])
+            return;
+        }
+
+        
+        navigate('/')
+    }
 
     return(
         <main>
+            {err.length > 0 
+            ?
+            err.map(item => <div key={item.message}>{item.message}</div>)
+            :
+            <></>
+            }
+            
             <div className={styles["register-form"]}>
                 <h1 >Registration Form</h1>
                 <form className= {styles["register-form-form"]} onSubmit={submitHandler}>
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -4,7 +4,16 @@ import {useNavigate} from 'react-router-dom'
 export default function useForm(formType,initialValue,onSubmitHandler){
     let [value,setValue] = useState(initialValue);
     let navigate = useNavigate();
-    let changeHandler = (e) => {
+    let changeHandler = (e,err) => {
+        if (!!err){
+        if (formType == 'register' && err.length > 0){
+            setValue(state => ({
+                email: value.email,
+                password:'',
+                repass : ''
+            }))
+        }
+        }
         setValue(state => ({
             ...state,
             [e.target.name]: e.target.value,
@@ -14,38 +23,38 @@ export default function useForm(formType,initialValue,onSubmitHandler){
     async function submitHandler(e){
         e.preventDefault();
         
-        if (formType == `register`){
-            let pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
+        // if (formType == `register`){
+        //     let pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
 
-            if (value.email.trim() == '' || value.password.trim() == ''){
-                return alert('All fields are required!');
-            }
-            if(!pattern.test(value.email.trim())){
-                return alert('You have to type a valid email!');
-            }
-            if(value.password.trim().length < 4){
-                return alert('Your password must be at least 4 symbols long!');
-            }
-            if(value.password.trim() != value.repass.trim()){
-                return alert('Your passwords should match!');
-            }
-            let {email,password} = value;
+        //     if (value.email.trim() == '' || value.password.trim() == ''){
+        //         return alert('All fields are required!');
+        //     }
+        //     if(!pattern.test(value.email.trim())){
+        //         return alert('You have to type a valid email!');
+        //     }
+        //     if(value.password.trim().length < 4){
+        //         return alert('Your password must be at least 4 symbols long!');
+        //     }
+        //     if(value.password.trim() != value.repass.trim()){
+        //         return alert('Your passwords should match!');
+        //     }
+        //     let {email,password} = value;
 
-            email.trim();
-            password.trim();
+        //     email.trim();
+        //     password.trim();
 
-            let data = await onSubmitHandler(email,password);
+        //     let data = await onSubmitHandler(email,password);
 
-            if (data.message){
-                return alert(data.message);
-            }
-            setValue({
-                email: '',
-                password: '',
-                repass: ''
-            })
-            navigate('/')
-        }
+        //     if (data.message){
+        //         return alert(data.message);
+        //     }
+        //     setValue({
+        //         email: '',
+        //         password: '',
+        //         repass: ''
+        //     })
+        //     navigate('/')
+        // }
         
         if (formType == `login`){
             if (value.email.trim() == '' || value.password.trim() == ''){
