7a2879519853fe0b8c6b6e89796ec24b37d19f76
icovv <hvasilev00@abv.bg>
2024-07-31 20:36:07 +0300

    Fixing background image in the home page

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -5,12 +5,12 @@ import { useEffect } from 'react'
 
 
 export default function Home() {
-    useEffect(()=>{
-        document.body.style.backgroundImage = `url(${background})`
-    },[])
+    // useEffect(()=>{
+    //     document.body.style.backgroundImage = `url(${background})`
+    // },[])
     return (
-        <main>
-        <div className= {styles['home-text']}>
+        <main style={{backgroundImage:`url(${background})`, position: 'fixed', backgroundSize:'cover', width: "100%", height: "100%"}}>
+        <div className= {styles['home-text']} >
             <h1>Show us your car</h1>
             <Link to="/catalog" className={styles['catalog-btn-home']}>Catalog</Link>
         </div>
