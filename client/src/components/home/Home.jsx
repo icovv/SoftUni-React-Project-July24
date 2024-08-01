94fb66b531c90b112c14ff77cb8ed9a2f62d36fd
icovv <hvasilev00@abv.bg>
2024-08-01 22:37:39 +0300

    clean up imports in Home component which are not required

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,13 +1,9 @@
 import { Link } from 'react-router-dom'
 import styles from './Home.module.css'
 import background from '/images/nackground.jpg'
-import { useEffect } from 'react'
 
 
 export default function Home() {
-    // useEffect(()=>{
-    //     document.body.style.backgroundImage = `url(${background})`
-    // },[])
     return (
         <main style={{backgroundImage:`url(${background})`, position: 'fixed', backgroundSize:'cover', width: "100%", height: "100%"}}>
         <div className= {styles['home-text']} >
